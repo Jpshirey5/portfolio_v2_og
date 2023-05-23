e64d824e9c8be8f0e6e85a3aeb82a74d29bc1bcb
jpshirey5 <jayshirey@icloud.com>
2023-05-23 12:55:21 -0400

    main intro section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Components/header';
+import Main from './Components/main';
 
 export default function App() {
   return (
     <div className="Header">
         <Header />
+        <div className='main'>
+          <Main />
+        </div>
     </div>
     
   );
---
+++ src/Components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <div className='Intro'>
+            <h3>Hi, my name is</h3>
+            <h1>John Shirey</h1>
+            <h1>I build things to help you change the World.</h1>
+            <h4>I am a Full Stack Developer that specializes in strategic front-end design with React. 
+                I am focused on helping you create, design and develop human-centered experiences on the web.</h4>
+        </div>
+    );
+}
